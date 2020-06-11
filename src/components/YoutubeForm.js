7e0d20f1d32af43bab30841a5a58c0a40fcf8f0f
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-10 21:49:46 -0500

    Managinf state with formik 😀

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import { useFormik } from 'formik';
 
 const YoutubeForm = () => {
+  const Formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      channel: '',
+    },
+  });
+
+  // console.log('Form initial values 😉', Formik.values);
   return (
     <div className='container'>
       <form>
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' name='name' />
+        <input
+          type='text'
+          id='name'
+          name='name'
+          onChange={Formik.handleChange}
+          value={Formik.values.name}
+        />
         <label htmlFor='email'>Email</label>
-        <input type='email' id='email' name='email' />
+        <input
+          type='email'
+          id='email'
+          name='email'
+          onChange={Formik.handleChange}
+          value={Formik.values.email}
+        />
         <label htmlFor='channel'>Channel</label>
-        <input type='text' id='channel' name='channel' />
+        <input
+          type='text'
+          id='channel'
+          name='channel'
+          onChange={Formik.handleChange}
+          value={Formik.values.channel}
+        />
         <button type='submit'>Submit</button>
       </form>
     </div>
