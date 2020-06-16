504e6012ba34fc6e487183a3324e9c7a0933c693
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-15 21:28:16 -0500

    disabling submit 🥱

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -168,7 +168,9 @@ const YoutubeForm = () => {
             >
               Visit all
             </button>
-            <button type='submit'>Submit</button>
+            <button type='submit' disabled={!formik.isValid}>
+              Submit
+            </button>
           </Form>
         );
       }}
